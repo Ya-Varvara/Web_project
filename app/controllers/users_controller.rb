21a0c40fe92aedcfcd7c82ca5fa1733bf57cf9e4
Varvara Yakovleva <yakovleva.varvaraa@gmail.com>
2021-12-23 23:59:28 +0300

    Fixed new recipe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
         format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
         format.json { render root_path, status: :created, location: @user }
       else
-        format.html { render :new_old, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
