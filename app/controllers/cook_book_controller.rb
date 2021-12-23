fef52b8538d03e262adee25f2a6889092d19d1f4
S.Kretov <s.kretov@a1qa.com>
2021-12-24 02:00:57 +0300

    Last changes

--- app/controllers/cook_book_controller.rb
+++ app/controllers/cook_book_controller.rb
@@ -2,13 +2,8 @@ class CookBookController < ApplicationController
   skip_before_action :require_login, only: %i[main_page cook_page]
 
   def main_page
-
-  end
-
-  def user_page
-  end
-
-  def cook_page
+    @recipes = Recipe.all.map { |elem| { id: elem.id, name: elem.name, ingred: elem.ingred, descrip: elem.descrip, time: elem.time, username: User.find_by(id: elem.user_id).username } }.each_slice(2).map
+    p @recipes
 
   end
 end
